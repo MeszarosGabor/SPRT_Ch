37f0c30985ec4fcbfea45ce3cc182f93f9180afa
Gabor Meszaros <jegkocka88@gmail.com>
2022-05-15 15:10:45 +0200

    Performance improvement: make moderator calls parallel
    Parallel querying of the paragraphs with IO wait.

--- language_moderator.py
+++ language_moderator.py
@@ -1,4 +1,8 @@
+from gevent import monkey
+monkey.patch_all()
+
 # Third party imports
+import gevent
 import requests
 
 
@@ -9,10 +13,11 @@ MODERATION_TIMEOUT_SEC = 5
 def moderate_sentence(sentence):
     data = {"fragment": sentence}
     try:
-        # resp = requests.post(MODERATOR_ENDPOINT,
-        #                      json=data,
-        #                      timeout=MODERATION_TIMEOUT_SEC)
-        resp = requests.post(MODERATOR_ENDPOINT)
+        resp = requests.post(MODERATOR_ENDPOINT,
+                             json=data,
+                             timeout=MODERATION_TIMEOUT_SEC)
+        if resp.status_code != 200:
+            return  {"has_foul_language": None}
         return resp
     except requests.exceptions.RequestException as e:
         return {"has_foul_language": None}
@@ -20,14 +25,15 @@ def moderate_sentence(sentence):
 
 def moderate_entry(entry):
     entry_has_foul = False
+    moderator_greenlets = []
     for paragraph in entry["paragraphs"]:
-        sentence_has_foul = moderate_sentence(paragraph).json().get("has_foul_language")
-        if sentence_has_foul:
-            entry_has_foul = True
-            break
-        elif sentence_has_foul is None:
-            # failed to verify a sentence;
-            # we can only return None/True from now on.
-            entry_has_foul = None
+        moderator_greenlets.append(gevent.spawn(moderate_sentence, paragraph))
+    gevent.joinall(moderator_greenlets)
+    responses = [r.value.get("has_foul_language") for r in moderator_greenlets]
+
+    if True in responses:
+        entry_has_foul = True
+    elif None in responses:
+        entry_has_foul = None
     entry["has_foul_language"] = entry_has_foul
     return entry
